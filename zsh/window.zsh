9cf625cac1df68afdd3829ba08c7c78cb6604018
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-01-01 19:18:13 +0100

    fix todo for missing window zoom and vim suspended title in tmux

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -26,37 +26,31 @@ function get_host_from_ssh_config() {
 }
 
 function title() {
-  case $TERM in
-  screen|screen-256color)
-    precmd () {
-      local prefix
-      prefix=""
-      if is-vi-suspended; then
-        prefix="(vim):"
-      fi
-      print -Pn "\ek$prefix%21<...<%~\e\\" # screen title (in ^A")
-    }
-    preexec () {
-      local command
-      local formatted_title
-      # only print name of command without arguments
-      command=$(echo "$1" | cut -d" " -f1)
-      formatted_title="${command/\(//}:%21<...<%~"
-      if [[ $command == "vim" ]]; then
-        export VIM_TITLE=$formatted_title
-      fi
-      if [[ $command == "ssh" ]]; then
-        formatted_title="$(get_host_from_ssh "$1")"
-      fi
-      print -Pn "\ek$formatted_title\e\\" # screen title (in ^A")
-      # TODO disentangle from the reset
-      _refresh_envvariable_from_tmux
-    }
-    ;;
-  xterm*|rxvt)
-    print -Pn "\e]2;%m:%21<...<%~\a" # print hostname:pwd for xterm title
-    ;;
-  esac
+  precmd () {
+    local prefix
+    prefix=""
+    if is-vi-suspended; then
+      prefix="(vim):"
+    fi
+    print -Pn "\ek$prefix%21<...<%~\e\\" # screen title (in ^A")
+  }
+  preexec () {
+    local command
+    local formatted_title
+    # only print name of command without arguments
+    command=$(echo "$1" | cut -d" " -f1)
+    formatted_title="${command/\(//}:%21<...<%~"
+    if [[ $command == "vim" ]]; then
+      export VIM_TITLE=$formatted_title
+    fi
+    if [[ $command == "ssh" ]]; then
+      formatted_title="$(get_host_from_ssh "$1")"
+    fi
+    print -Pn "\ek$formatted_title\e\\" # screen title (in ^A")
+    # TODO disentangle from the reset
+    _refresh_envvariable_from_tmux
+  }
+  print -Pn "\e]2;%m:%21<...<%~\a" # print hostname:pwd for xterm title
 }
 
 function restore_vim_title() {
