42a96031a3b6944797dc6bf825ae11ec68bf1cbd
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-11-14 17:38:07 +0100

    try testing virtualenv

--- virtualenvwrapper/test.sh
+++ virtualenvwrapper/test.sh
@@ -5,3 +5,12 @@ which mkvirtualenv
 
 echo "Check if 'workon' is available"
 which workon
+
+echo "Test create virtualenv, activate it"
+mkvirtualenv mytest
+workon mytest
+if [[ -z ${VIRTUAL_ENV} ]]; then
+  echo "Could not activate virtualenv"
+  exit 1
+fi
+
--- virtualenvwrapper/workon.zsh
+++ virtualenvwrapper/workon.zsh
@@ -1,5 +1,6 @@
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Devel
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 # Missing expansion of VIRTUALENVWRAPPER_PYTHON
 # This needs to be specific to each platform, i.e., where python3 sits.
 # Set this instead in your .zprofile
