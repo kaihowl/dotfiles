f2c7dc6012a98eb9b165b6b2d99537b4ff3a3d25
Kai Howelmeyer <kai@hoewelmeyer.eu>
2021-11-14 10:51:52 +0100

    fix: install missing python3-dev (#73)
    For example ujson as part of pyls needs to be build for Hirsute. This
    requires the python3 development headers.
    
    Fixes: #70

--- common/pip.sh
+++ common/pip.sh
@@ -3,7 +3,7 @@ function ensure_pip_installed() {
     if test ! "$(python3 -m pip)"
     then
       source $DOTS/common/apt.sh
-      apt_install --no-install-recommends python3-pip
+      apt_install --no-install-recommends python3-pip python3-dev
     fi
   fi
 }
