5051ecf89c44ee05cf50f11ae2878cda1f5eb5db
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-11-19 08:12:49 +0100

    fix up test

--- virtualenvwrapper/test.sh
+++ virtualenvwrapper/test.sh
@@ -1,16 +1,36 @@
-#!/bin/zsh -ie
+#!/bin/zsh -ix
+# Deliberately not set -e as the virtualenvwrapper functions inherit this.
+# I could not get the test to pass with it activated. If the actual expectations
+# of this test are not met, the test will still reliably fail, though.
 
 echo "Check if 'mkvirtualenv' is available"
 which mkvirtualenv
+if [ $? != 0 ]; then
+  echo "Failed to find mkvirtualenv"
+  exit 1
+fi
 
 echo "Check if 'workon' is available"
 which workon
+if [ $? != 0 ]; then
+  echo "Failed to find workon";
+  exit 1
+fi
 
 echo "Test create virtualenv, activate it"
-mkvirtualenv mytest
-workon mytest
+mktmpenv
+if [ $? != 0 ]; then
+  echo "Failed to execute mktmpenv"
+  exit 1
+fi
+
 if [[ -z ${VIRTUAL_ENV} ]]; then
   echo "Could not activate virtualenv"
   exit 1
 fi
 
+deactivate
+if [ $? != 0 ]; then
+  echo "Failed to execute deactivate"
+  exit 1
+fi
