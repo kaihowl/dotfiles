b93030f7fc395a0453dc0d90089852b459d252c2
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-11-20 12:34:10 +0100

    set VIRTUALENVWRAPPER_PYTHON for macos as well

--- virtualenvwrapper/install.sh
+++ virtualenvwrapper/install.sh
@@ -4,10 +4,11 @@ set -e
 if [ "$(uname -s)" = "Darwin" ]; then
   sudo python3 -m ensurepip --upgrade
   sudo python3 -m pip install --upgrade virtualenvwrapper
-  touch ~/.zprofile
-  sed -i ".backup" "/^export VIRTUALENVWRAPPER_PYTHON.*$/d" ~/.zprofile
-  echo "export VIRTUALENVWRAPPER_PYTHON=$(which python3)" >> ~/.zprofile
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   source $DOTS/common/apt.sh
   apt_install python3-virtualenvwrapper
 fi
+
+touch ~/.zprofile
+sed -i ".backup" "/^export VIRTUALENVWRAPPER_PYTHON.*$/d" ~/.zprofile
+echo "export VIRTUALENVWRAPPER_PYTHON=$(which python3)" >> ~/.zprofile
