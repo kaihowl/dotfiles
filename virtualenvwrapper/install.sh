265b3368a25aacdcdc97a6b6f7b487655ab80fc6
Kai Howelmeyer <kai@hoewelmeyer.eu>
2021-11-23 08:34:55 +0100

    fix: virtualenv (#78)
    Detect and use correct python3 during installation. Add test.

--- virtualenvwrapper/install.sh
+++ virtualenvwrapper/install.sh
@@ -8,3 +8,10 @@ elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   source $DOTS/common/apt.sh
   apt_install python3-virtualenvwrapper
 fi
+
+# Not using in place editing as macOS and GNU sed
+# have incompatible -i options. 
+touch ~/.zprofile
+sed "/^export VIRTUALENVWRAPPER_PYTHON.*$/d" ~/.zprofile > ~/.zprofilenew
+echo "export VIRTUALENVWRAPPER_PYTHON=$(which python3)" >> ~/.zprofilenew
+mv ~/.zprofilenew ~/.zprofile 
---
+++ virtualenvwrapper/test.sh
@@ -0,0 +1,40 @@
+#!/bin/zsh -lix
+# Deliberately not set -e as the virtualenvwrapper functions inherit this.
+# I could not get the test to pass with it activated. If the actual expectations
+# of this test are not met, the test will still reliably fail, though.
+# This has to be a login shell to read .zprofile.
+# This has to be an interactive shell to read workon.zsh.
+
+echo "Check: VIRTUALENVWRAPPER_PYTHON: ${VIRTUALENVWRAPPER_PYTHON}"
+
+echo "Check if 'mkvirtualenv' is available"
+which mkvirtualenv
+if [ $? != 0 ]; then
+  echo "Failed to find mkvirtualenv"
+  exit 1
+fi
+
+echo "Check if 'workon' is available"
+which workon
+if [ $? != 0 ]; then
+  echo "Failed to find workon";
+  exit 1
+fi
+
+echo "Test create virtualenv, activate it"
+mktmpenv
+if [ $? != 0 ]; then
+  echo "Failed to execute mktmpenv"
+  exit 1
+fi
+
+if [[ -z ${VIRTUAL_ENV} ]]; then
+  echo "Could not activate virtualenv"
+  exit 1
+fi
+
+deactivate
+if [ $? != 0 ]; then
+  echo "Failed to execute deactivate"
+  exit 1
+fi
