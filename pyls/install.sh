400bd67e97cb85e9917ba5dfab31f753249edbca
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-11-24 19:43:20 +0100

    fix: update pylsp to the maintained fork
    Fixes #80

--- pyls/install.sh
+++ pyls/install.sh
@@ -4,4 +4,4 @@ source $DOTS/common/pip.sh
 ensure_pip_installed
 
 # Install autopep8 to activate optional source formatting in pyls
-sudo python3 -m pip install --upgrade python-language-server[all] autopep8
+sudo python3 -m pip install --upgrade python-lsp-server[all] autopep8
