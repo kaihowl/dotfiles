38f4ddc8d44ecc1ccd69f9ae4ebeeac0d5668e4e
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-11-30 08:08:29 +0100

    feat: install and use 'just'
    Fixes #77

---
+++ just/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+cd "$(dirname "$0")"
+
+if [ "$(uname)" == "Darwin" ]; then
+  source $DOTS/common/brew.sh
+  brew_install just
+elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
+  source $DOTS/common/apt.sh
+  apt_install curl
+  tmpfile=$(mktemp)
+  trap "rm -rf ${tmpfile}" EXIT
+  curl -Lo "${tmpfile}" https://github.com/casey/just/releases/download/0.10.4/just-0.10.4-x86_64-unknown-linux-musl.tar.gz
+  expect_hash="3898b3b4d34f9f7d80be578f4159e3c8000d9b08"
+  actual_hash="$(sha1sum ${tmpfile} | cut -d' ' -f 1)"
+  if [[ "$expect_hash" != "$actual_hash" ]]; then
+    echo "sha1sum mismatch for just. Aborting."
+    exit 1
+  fi
+  mkdir -p ~/bin
+  echo "just" | tar -C ~/bin --extract -z -f "${tmpfile}" --files-from=- -C ~/bin
+fi
+
