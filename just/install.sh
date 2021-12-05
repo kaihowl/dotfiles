8bf7dad26da6e5c4c08617bc46eb7d4f179fc9d8
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-12-05 12:34:09 +0100

    fix: deprecate sha1sum, use sha256
    Fixes #90

--- just/install.sh
+++ just/install.sh
@@ -11,10 +11,10 @@ elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   tmpfile=$(mktemp)
   trap "rm -rf ${tmpfile}" EXIT
   curl -Lo "${tmpfile}" https://github.com/casey/just/releases/download/0.10.4/just-0.10.4-x86_64-unknown-linux-musl.tar.gz
-  expect_hash="3898b3b4d34f9f7d80be578f4159e3c8000d9b08"
-  actual_hash="$(sha1sum ${tmpfile} | cut -d' ' -f 1)"
+  expect_hash="4d1f3e3bef97edeee26f1a3760ac404dcb3a1f52930405c8bd3cd3e5b70545d8"
+  actual_hash="$(shasum -a 256 ${tmpfile} | cut -d' ' -f 1)"
   if [[ "$expect_hash" != "$actual_hash" ]]; then
-    echo "sha1sum mismatch for just. Aborting."
+    echo "shasum mismatch for just. Aborting."
     exit 1
   fi
   mkdir -p ~/bin
