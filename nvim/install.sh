bda938233f3183e4c97147eabba363e19c32063c
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-12-05 10:43:28 +0000

    refactor

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,38 +1,27 @@
 #!/bin/bash -ex
 
 if [ "$(uname -s)" = "Darwin" ]; then
-  tmpfile=$(mktemp)
-  trap "rm -rf ${tmpfile}" EXIT
-  curl -Lo "${tmpfile}" https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-macos.tar.gz
+  download_url="https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-macos.tar.gz"
   expect_hash="03cdbfeec3493f50421a9ae4246abe4f9493715f5e151a79c4db79c5b5a43acc"
-  actual_hash="$(shasum -a 256 ${tmpfile} | cut -d' ' -f 1)"
-  if [[ "$expect_hash" != "$actual_hash" ]]; then
-    echo "shasum mismatch for nvim. Aborting."
-    exit 1
-  fi
-  mkdir -p ~/.nvim
-  tar -C ~/.nvim --extract -z -f "${tmpfile}" --strip-components 1
-
-  # Make freshly installed nvim available in path
-  script_dir=$(dirname $0)
-  . ${script_dir}/path.zsh
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  tmpfile=$(mktemp)
-  trap "rm -rf ${tmpfile}" EXIT
-  curl -Lo "${tmpfile}" https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-linux64.tar.gz
+  download_url="https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-linux64.tar.gz"
   expect_hash="9a7f72e25747c3839f2c8978ef4f902aada0c60ad4b5ff0cb8b9d4c1f0b35586"
-  actual_hash="$(shasum -a 256 ${tmpfile} | cut -d' ' -f 1)"
-  if [[ "$expect_hash" != "$actual_hash" ]]; then
-    echo "shasum mismatch for nvim. Aborting."
-    exit 1
-  fi
-  mkdir -p ~/.nvim
-  tar -C ~/.nvim --extract -z -f "${tmpfile}" --strip-components 1
-
-  # Make freshly installed nvim available in path
-  script_dir=$(dirname $0)
-  . ${script_dir}/path.zsh
 fi
+
+tmpfile=$(mktemp)
+trap "rm -rf ${tmpfile}" EXIT
+curl -Lo "${tmpfile}" "${download_url}"
+actual_hash="$(shasum -a 256 ${tmpfile} | cut -d' ' -f 1)"
+if [[ "$expect_hash" != "$actual_hash" ]]; then
+  echo "shasum mismatch for nvim. Aborting."
+  exit 1
+fi
+mkdir -p ~/.nvim
+tar -C ~/.nvim --extract -z -f "${tmpfile}" --strip-components 1
+
+# Make freshly installed nvim available in path
+script_dir=$(dirname $0)
+. ${script_dir}/path.zsh
 
 source $DOTS/common/pip.sh
 ensure_pip_installed
