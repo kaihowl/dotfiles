e9fa8322e94ecd948d65918fc257982374528c80
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-12-23 10:57:57 +0100

    feat(bin): measure-runtime with `-n`, make expected-ms optional

--- bin/measure-runtime.py
+++ bin/measure-runtime.py
@@ -7,7 +7,7 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--expected-ms', type=int)
-parser.add_argument('--repeat', type=int, default=10)
+parser.add_argument('-n', '--repeat', type=int, default=10)
 parser.add_argument('command', nargs=argparse.REMAINDER)
 args = parser.parse_args()
 
@@ -22,6 +22,6 @@ min_measurements = min(timings)
 
 print("min in {} runs was: {} ms".format(args.repeat, min_measurements))
 
-if min_measurements > args.expected_ms:
+if args.expected_ms and min_measurements > args.expected_ms:
     print("Expected was: {} ms".format(args.expected_ms))
     sys.exit(1)
