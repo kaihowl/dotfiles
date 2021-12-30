ca2c05355a5d7cdf139487a991cff32900823ea0
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2021-12-30 17:08:39 +0100

    draft: use fzf for zoxide

--- zoxide/load.zsh
+++ zoxide/load.zsh
@@ -2,3 +2,10 @@
 export _ZO_ECHO=1
 
 eval "$(zoxide init --cmd j zsh)"
+
+# TODO(kaihowl) Depends on fzf completions internals
+# Enable fzf completion on "j <prefix>**"
+function _fzf_complete_j() {
+  # shellcheck disable=SC2154
+  zoxide query -i "${prefix}"
+}
