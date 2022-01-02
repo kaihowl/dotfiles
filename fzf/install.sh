ce14e05409a5ec603668b9f8516a3cbc053722ac
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-01-02 17:41:14 +0100

    fix: update fzf on install
    Fix #184

--- fzf/install.sh
+++ fzf/install.sh
@@ -3,9 +3,6 @@ set -ex
 
 cd "$(dirname "$0")"
 
-# Depends on an installed zsh, otherwise the zsh configuration is not generated
-../zsh/install.sh
-
 if [ "$(uname)" == "Darwin" ]; then
   source "$DOTS/common/brew.sh"
   brew_install curl wget
@@ -17,4 +14,5 @@ fi
 if [ ! -d ~/.fzf ]; then
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 fi
-~/.fzf/install --all
+(cd ~/.fzf && git pull --rebase)
+~/.fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish
