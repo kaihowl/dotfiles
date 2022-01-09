c518bca32999870af0a491d90908e99a0ca1f276
Kai Howelmeyer <kai@hoewelmeyer.eu>
2022-01-09 18:28:05 +0100

    ci: annotate deprecation/warning/errors (#227)
    Fixes #226

---
+++ script/ci.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ $(uname) == *Darwin* ]]; then
+  word_begin="[[:<:]]"
+  word_end="[[:>:]]"
+  # Use line buffering
+  less_buffering="-l"
+  # Do not use stdbuf (not available by default, not needed so far.)
+  stdbuf=()
+else
+  word_begin="\b"
+  word_end="\b"
+  # Do not buffer
+  less_buffering="-u"
+  stdbuf=(stdbuf -oL -eL)
+fi
+
+function decorate() {
+  sed "${less_buffering}" \
+    -e "s/^.*${word_begin}[dD]eprecat.*$/::notice:: &/" \
+    -e "s/^.*${word_begin}[wW]arning${word_end}.*$/::warning:: &/" \
+    -e "s/^.*${word_begin}[eE]rror${word_end}.*$/::error:: &/"
+}
+
+"${stdbuf[@]}" ./script/bootstrap > >(decorate) 2>&1
+"${stdbuf[@]}" ./script/install > >(decorate) 2>&1
+"${stdbuf[@]}" ./script/test > >(decorate) 2>&1
