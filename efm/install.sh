3b21233de3eb0e78a7c3d4d21738abad868d5646
Kai Hoewelmeyer <kai.hoewelmeyer@tomtom.com>
2022-01-12 18:27:07 +0100

    chore(efm): update efm-langserver

--- efm/install.sh
+++ efm/install.sh
@@ -2,11 +2,11 @@
 set -ex
 
 if [ "$(uname -s)" = "Darwin" ]; then
-  download_url="https://github.com/mattn/efm-langserver/releases/download/v0.0.38/efm-langserver_v0.0.38_darwin_amd64.zip"
-  expect_hash="89d42660ffa2d0642e709bfb00f5a8fa5a46b4f3b188632f778541f5709e4c28"
+  download_url="https://github.com/mattn/efm-langserver/releases/download/v0.0.40/efm-langserver_v0.0.40_darwin_amd64.zip"
+  expect_hash="9310b419e91423e398d1078bbaf24cfc95f0e50bccdbd5b30b69118f224592b7"
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  download_url="https://github.com/mattn/efm-langserver/releases/download/v0.0.38/efm-langserver_v0.0.38_linux_amd64.tar.gz"
-  expect_hash="a3e2ae7c28037c809478aa96f68049db7672e04f755a1d84a26a54e4a7d307dd"
+  download_url="https://github.com/mattn/efm-langserver/releases/download/v0.0.40/efm-langserver_v0.0.40_linux_amd64.tar.gz"
+  expect_hash="101be2f9ed8de0357363f90868442802c8ea0be4f1b8f1e0ad327e3923e9fe31"
 fi
 
 tmpfile=$(mktemp)
