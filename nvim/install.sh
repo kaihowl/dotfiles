61707d16196bbe61d0130f5683a6743204b96dec
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-01-31 21:40:24 +0100

    fix(nvim): Do not attempt to load plugins that are not yet installed
    Fixes #230

--- nvim/install.sh
+++ nvim/install.sh
@@ -35,5 +35,5 @@ plug_dir="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
 if [ ! -d "$plug_dir" ]; then
   curl -fLo "${plug_dir}" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-nvim --headless +PlugUpgrade +PlugUpdate +qall
 
+nvim -u NONE --headless -c 'let g:first_time_startup=1' -c 'source ~/.config/nvim/init.vim' +PlugUpgrade +PlugUpdate +qall
