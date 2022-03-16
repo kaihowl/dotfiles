970f26bf49bac58e8e2ee0da948c5ee230111d54
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-03-16 07:47:49 +0100

    test(tmux): Add basic configuration test for tmux
    Fixes #310

---
+++ tmux/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo "Starting tmux background session"
+tmux -D &
+tmux_pid=$!
+
+function kill_tmux {
+  kill ${tmux_pid}
+  wait ${tmux_pid}
+}
+
+trap kill_tmux EXIT
+
+echo "Waiting for tmux to be started..."
+while ! tmux list-sessions; do
+  sleep 1
+done
+
+echo "Source tmux configuration to check for errors"
+tmux source "$DOTS/tmux/tmux.conf.symlink"
+
+echo "Success"
