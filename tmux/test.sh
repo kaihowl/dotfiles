dab3e677189b7121f07612a39020dcb3838a57f8
Kai Howelmeyer <kai@hoewelmeyer.eu>
2022-03-17 11:32:22 +0100

    test(tmux): Add basic configuration test for tmux (#312)
    Fixes #310

---
+++ tmux/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+echo "Starting tmux background session"
+tmux -D &
+tmux_pid=$!
+
+function kill_tmux {
+  kill ${tmux_pid} || true
+  wait ${tmux_pid} || true
+}
+
+trap kill_tmux EXIT
+
+echo "Waiting for tmux to be started..."
+while ! tmux list-sessions; do
+  if ! ps -p $tmux_pid; then
+    echo "No support for supervised/non-daemonized tmux"
+    echo "Skipping test"
+    exit 0
+  fi
+  sleep 1
+done
+
+echo "Source tmux configuration to check for errors"
+tmux source "$DOTS/tmux/tmux.conf.symlink"
+
+echo "Success"
