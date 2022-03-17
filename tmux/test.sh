0da051a0dd33c200e5fe28967ace55bbf47bc6c6
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-03-17 11:02:46 +0100

    check compatibility with directly starting tmux and backgrounding it

--- tmux/test.sh
+++ tmux/test.sh
@@ -1,10 +1,4 @@
-#!/bin/zsh
-
-if ! tmux -D -c 'exit'; then
-  echo "No support for supervised/non-daemonized tmux"
-  echo "Skipping test"
-  exit 0
-fi
+#!/bin/bash
 
 set -e
 
@@ -13,14 +7,19 @@ tmux -D &
 tmux_pid=$!
 
 function kill_tmux {
-  kill ${tmux_pid}
-  wait ${tmux_pid}
+  kill ${tmux_pid} || true
+  wait ${tmux_pid} || true
 }
 
 trap kill_tmux EXIT
 
 echo "Waiting for tmux to be started..."
 while ! tmux list-sessions; do
+  if ! ps -p $tmux_pid; then
+    echo "No support for supervised/non-daemonized tmux"
+    echo "Skipping test"
+    exit 0
+  fi
   sleep 1
 done
 
