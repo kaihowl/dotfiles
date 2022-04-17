159773b27d1b95886d2d38fe0b7d4d60452d383e
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-04-17 09:13:13 +0200

    ci: track performance with git-perf 0.2.0

---
+++ script/perf.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh -i
+
+set -e
+set -x
+# TODO(kaihowl) no subst from script available
+PATH=$(pwd)/bin:$PATH
+export PATH
+python3 -m pip install -r bin/git-perf-requirements.txt
+git perf measure -n 10 -kv "os=${RUNNER_OS}" -m nvim -- nvim +qall
+git perf measure -n 10 -kv "os=${RUNNER_OS}" -m zsh -- zsh -i -c 'exit'
+git perf push
+
+set +e
+git perf audit -n 40 -m nvim -s "os=${RUNNER_OS}"
+nvim_exit=$?
+git perf audit -n 40 -m zsh -s "os=${RUNNER_OS}"
+zsh_exit=$?
+
+if [[ $zsh_exit -ne 0 ]] || [[ $nvim_exit -ne 0 ]]; then
+  echo $zsh_exit
+  echo $nvim_exit
+  exit 1
+fi
