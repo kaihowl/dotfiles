a4eeeb745b4dd68c94de6e2bee01798881777f0e
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-05-05 10:03:58 +0200

    feat: install jq

--- system/install.sh
+++ system/install.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ "$(uname -s)" = "Darwin" ]; then
   source "$DOTS/common/brew.sh"
-  brew_install ncdu tree
+  brew_install ncdu tree jq
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   source "$DOTS/common/apt.sh"
-  apt_install ncdu tree
+  apt_install ncdu tree jq
 fi
