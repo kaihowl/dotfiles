d08d5f0af00f28a591301af8bbd741027ed8fa8e
Kai Howelmeyer <kai@hoewelmeyer.eu>
2022-05-06 16:32:32 +0200

    feat: report all installed versions (#366)
    * feat: report all installed versions
    
    Fixes #345
    
    accept-perf: ci
    
    * Update versions.sh
    
    * Update test.yml
    
    * Update test.yml
    
    * Update test.yml

---
+++ script/versions.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh -i 
+set -e
+set -x
+
+if [ $# != 1 ]; then
+  echo Missing output file argument
+  exit 1
+fi
+
+output_file=$1
+echo "output to $output_file"
+
+echo "" > "$output_file"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  echo "brew installed:" >> "$output_file"
+  brew info --installed --json | jq '.[] | .name + "@" + .installed[0].version' | tee -a "$output_file"
+elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
+  echo "apt installed:" >> "$output_file"
+  sudo apt-get list --installed | tee -a "$output_file"
+fi
+
+tmp_file=$(mktemp)
+trap 'rm -rf $tmp_file' EXIT
+nvim --headless "+PlugSnapshot! $tmp_file" +qall
+
+echo "nvim plugins installed:" >> "$output_file"
+cat "$tmp_file" >> "$output_file"
