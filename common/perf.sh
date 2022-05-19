6e7d60d804e19555e01ea8b9a8eaebe96614fb4b
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-05-19 20:31:46 +0200

    refactor: move performance testing into ci script
    This has the minor effect of factoring the measurement time into the
    reported CI time as well.
    
    This prepares the stage to allow tracking the performance of various
    substeps and to be able to easily inject more context info from the
    GitHub runner, e.g., the runner's name.

---
+++ common/perf.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+set -u
+
+export PATH=~/.local/bin:$PATH
+python3 -m pip install git+https://github.com/kaihowl/git-perf.git@latest
+
+function add_measurement {
+  if [[ $# -ne 2 ]]; then
+    echo "Expected name and value for add_measurement"
+    false
+  fi
+
+  name=$1
+  value=$2
+
+  git perf add -m "$name" -kv "os=${RUNNER_OS}" "$value"
+}
+
+function run_measurement {
+  echo $#
+  if [[ $# -lt 2 ]]; then
+    echo "Expected name and command for run_measurement"
+    false
+  fi
+
+  name=$1
+  git perf measure -n 10 -m "$name" -kv "os=${RUNNER_OS}" -- "${@:2}"
+}
+
+function publish_measurements {
+  git perf push
+}
+
+# Make functions available in called bash scripts as well
+export -f add_measurement
+export -f run_measurement
+export -f publish_measurements
--- script/ci.sh
+++ script/ci.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 if [[ $(uname) == *Darwin* ]]; then
   word_begin="[[:<:]]"
@@ -24,6 +25,36 @@ function decorate() {
     -e "s/^.*${word_begin}[eE]rror${word_end}.*$/::error:: &/"
 }
 
+source common/perf.sh
+START=$(date +%s)
+
 "${stdbuf[@]}" ./script/bootstrap > >(decorate) 2>&1
 "${stdbuf[@]}" ./script/install > >(decorate) 2>&1
 "${stdbuf[@]}" ./script/test > >(decorate) 2>&1
+
+run_measurement nvim nvim +qall
+run_measurement zsh zsh -i -c 'exit'
+
+END=$(date +%s)
+CI_DURATION=$((END - START))
+
+add_measurement ci $CI_DURATION
+
+publish_measurements
+
+set +e
+git perf audit -n 40 -d 6 -m nvim -s "os=${RUNNER_OS}"
+nvim_exit=$?
+git perf audit -n 40 -d 6 -m zsh -s "os=${RUNNER_OS}"
+zsh_exit=$?
+git perf audit -n 40 -d 6 -m ci -s "os=${RUNNER_OS}"
+ci_exit=$?
+set -e
+
+if [[ $zsh_exit -ne 0 ]] || [[ $nvim_exit -ne 0 ]] || [[ $ci_exit -ne 0 ]]; then
+  echo $zsh_exit
+  echo $nvim_exit
+  echo $ci_exit
+  exit 1
+fi
+
