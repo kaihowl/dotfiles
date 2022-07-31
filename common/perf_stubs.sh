e69ff48b5bccce1ca0ac65c2af0737b15e2c05ff
Kai Howelmeyer <kai@hoewelmeyer.eu>
2022-07-31 14:54:14 +0200

    fix(script): only use add_measurement in CI (#405)
    In local runs replace it with a no-op interface.
    
    Fixes #397

---
+++ common/perf_stubs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [ -n "$DOTS_PERF_EXPORTED" ]; then
+  return 0;
+fi
+
+function add_measurement() {
+  /usr/bin/true
+}
+
+function run_measurement {
+  /usr/bin/true
+}
+
+function publish_measurements {
+  /usr/bin/true
+}
+
+# Make functions available in called bash scripts as well
+export -f add_measurement
+export -f run_measurement
+export -f publish_measurements
+
+export DOTS_PERF_EXPORTED=true
