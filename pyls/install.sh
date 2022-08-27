6a08b38dde91b558036e8f5bed05198575e0400b
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-08-27 17:15:22 +0200

    fix(pyls): no need to install autopep8 explicitly
    This is included as an optional dependency in python-lsp-server when
    specifying `all`. An explicit install of the latest autopep8 can
    conflict with the version specified by python-lsp-server.

--- pyls/install.sh
+++ pyls/install.sh
@@ -5,4 +5,4 @@ source "$DOTS/common/pip.sh"
 ensure_pip_installed
 
 # Install autopep8 to activate optional source formatting in pyls
-sudo python3 -m pip install --upgrade "python-lsp-server[all]" autopep8
+sudo python3 -m pip install --upgrade "python-lsp-server[all]"
