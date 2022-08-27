5459d4694b96ad73ab30c30df43774929e9c5639
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-08-27 13:37:47 +0200

    no test for cmake on 22.04

--- cmake/test.sh
+++ cmake/test.sh
@@ -5,7 +5,8 @@ echo "Check if cmake is available"
 which cmake
 
 echo "Check that cmake is installed from llvm apt repo (Ubuntu-only)"
-if [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
+# TODO(#440) cmake not yet available for jammy
+if [[ "$(lsb_release -i)" == *"Ubuntu"* ]] && [[ "$(lsb_release -cs)" != *"jammy"* ]]; then
   apt policy cmake
   apt policy cmake | grep -F '**' -A1 | grep -F apt.kitware.com
 fi
