9de204670a72ef112f417c5bdece184505045460
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-09-09 07:47:32 +0200

    use venv instead

--- common/python.sh
+++ common/python.sh
@@ -12,23 +12,8 @@ function ensure_pip_installed() {
   fi
 }
 
-function ensure_virtualenv_installed() {
-  # TODO(kaihowl) there could be an old symlink in /usr/local/bin/virtualenv
-  # TODO(kaihowl) remove that
-  if test ! "$(python3 -m pip &> /dev/null)"; then
-    if [[ "$(uname)" == "Linux" && "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-      source "$DOTS/common/apt.sh"
-      apt_install --no-install-recommends python3-virtualenv
-    elif [ "$(uname -s)" = "Darwin" ]; then
-      source "$DOTS/common/brew.sh"
-      brew_install virtualenv
-    fi
-  fi
-}
-
 function install_in_virtualenv() {
   ensure_pip_installed
-  ensure_virtualenv_installed
-  python3 -m virtualenv ~/.virtualenvs/dotfiles-run
+  python3 -m venv ~/.virtualenvs/dotfiles-run
   ~/.virtualenvs/dotfiles-run/bin/python3 -m pip install --upgrade "$*"
 }
