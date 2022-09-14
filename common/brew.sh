5edfae9f2d1b49d0e1ccc68ae413e24476d81aba
Kai Hoewelmeyer <kai.hoewelmeyer@tomtom.com>
2022-09-14 20:36:01 +0200

    fix(homebrew): fix homebrew install

--- common/brew.sh
+++ common/brew.sh
@@ -5,7 +5,7 @@ function ensure_brew_installed() {
   if test ! "$(which brew)"
   then
     echo "  Installing Homebrew for you."
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/homebrew-install.log
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 }
 
