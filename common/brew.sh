821a4ba45cd44ad52ca5334c113df401b88b056a
Kai Hoewelmeyer <kai.hoewelmeyer@tomtom.com>
2022-09-14 21:02:05 +0200

    fix: brew install

--- common/brew.sh
+++ common/brew.sh
@@ -6,6 +6,8 @@ function ensure_brew_installed() {
   then
     echo "  Installing Homebrew for you."
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 }
 
