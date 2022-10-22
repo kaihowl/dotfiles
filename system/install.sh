a6c9545880fa21eee985193bc0d7acc21a644838
Kai Howelmeyer <kai@hoewelmeyer.eu>
2022-10-22 15:55:25 +0200

    feat(system): install automake, libtool, pkg-config (#494)
    This is for example assumed to be present on the host system for conan
    packages to compile. Let's install them by default.
    
    Fixes #467

--- system/install.sh
+++ system/install.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ "$(uname -s)" = "Darwin" ]; then
   source "$DOTS/common/brew.sh"
-  brew_install ncdu tree jq htop
+  brew_install ncdu tree jq htop automake libtool pkg-config
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   source "$DOTS/common/apt.sh"
-  apt_install ncdu tree jq htop
+  apt_install ncdu tree jq htop automake libtool pkg-config
 fi
