7e18886e45b16a1894655baaf18dfe20f7ce9f32
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2022-12-02 17:34:11 +0100

    test(cmake): test for upstream jammy package for cmake
    Fixes #440

--- cmake/test.sh
+++ cmake/test.sh
@@ -5,8 +5,7 @@ echo "Check if cmake is available"
 which cmake
 
 echo "Check that cmake is installed from llvm apt repo (Ubuntu-only)"
-# TODO(#440) cmake not yet available for jammy
-if [[ "$(lsb_release -i)" == *"Ubuntu"* ]] && [[ "$(lsb_release -cs)" != *"jammy"* ]]; then
+if [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   apt-cache policy cmake
   apt-cache policy cmake | grep -F '**' -A1 | grep -F apt.kitware.com
 fi
