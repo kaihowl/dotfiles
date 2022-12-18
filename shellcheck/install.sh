f6d32c22895db63ddd830c4d07f538882d2ba483
Kai Howelmeyer <kai@hoewelmeyer.eu>
2022-12-18 17:46:03 +0100

    chore: upgrade shellcheck to 0.9.0 (#512)
    * chore: upgrade shellcheck to 0.9.0
    
    * fix: shellcheck findings
    
    Shellcheck 0.9.0 finds one unquoted comparison and multiple false
    positives in control flow analysis.
    
    Fixes #510

--- shellcheck/install.sh
+++ shellcheck/install.sh
@@ -3,11 +3,11 @@ set -ex
 
 if [ "$(uname -s)" = "Darwin" ]; then
   # TODO(kaihowl) there is no arm64 support yet.
-  download_url="https://github.com/koalaman/shellcheck/releases/download/v0.8.0/shellcheck-v0.8.0.darwin.x86_64.tar.xz"
-  expect_hash="e065d4afb2620cc8c1d420a9b3e6243c84ff1a693c1ff0e38f279c8f31e86634"
+  download_url="https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.darwin.x86_64.tar.xz"
+  expect_hash="7d3730694707605d6e60cec4efcb79a0632d61babc035aa16cda1b897536acf5"
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  download_url="https://github.com/koalaman/shellcheck/releases/download/v0.8.0/shellcheck-v0.8.0.linux.x86_64.tar.xz"
-  expect_hash="ab6ee1b178f014d1b86d1e24da20d1139656c8b0ed34d2867fbb834dad02bf0a"
+  download_url="https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.linux.x86_64.tar.xz"
+  expect_hash="700324c6dd0ebea0117591c6cc9d7350d9c7c5c287acbad7630fa17b1d4d9e2f"
 fi
 
 tmpfile=$(mktemp)
