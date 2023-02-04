670ef141b41796523aa2e5028bd80be3f92848a2
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2023-02-04 23:31:06 +0100

    feat: store installed binaries in local cache
    Fixes #442

--- efm/install.sh
+++ efm/install.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 set -ex
 
+version=0.0.44
+
 if [ "$(uname -s)" = "Darwin" ]; then
   if [ "$(uname -m)" = "arm64" ]; then
-    download_url="https://github.com/mattn/efm-langserver/releases/download/v0.0.44/efm-langserver_v0.0.44_darwin_arm64.zip"
+    download_url="https://github.com/mattn/efm-langserver/releases/download/v${version}/efm-langserver_v${version}_darwin_arm64.zip"
     expect_hash="18858e84d31464a7fd7ae0040fd298002f616f39b94369a14027c6c66324acf0"
   else
-    download_url="https://github.com/mattn/efm-langserver/releases/download/v0.0.44/efm-langserver_v0.0.44_darwin_amd64.zip"
+    download_url="https://github.com/mattn/efm-langserver/releases/download/v${version}/efm-langserver_v${version}_darwin_amd64.zip"
     expect_hash="a94f4410d59d299cf73b3709e63f6ee9d86c1aff397c14b44275927a8312cc7a"
   fi
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  download_url="https://github.com/mattn/efm-langserver/releases/download/v0.0.44/efm-langserver_v0.0.44_linux_amd64.tar.gz"
+  download_url="https://github.com/mattn/efm-langserver/releases/download/v${version}/efm-langserver_v${version}_linux_amd64.tar.gz"
   expect_hash="ec426dd75cd3afa1f921a25c9cd635958091a1f2d4d7f747181eef8d355cb432"
 fi
 
-tmpfile=$(mktemp)
-# shellcheck disable=SC2064
-trap "rm -rf ${tmpfile}" EXIT
-curl -Lo "${tmpfile}" "${download_url}"
-actual_hash="$(shasum -a 256 "${tmpfile}" | cut -d' ' -f 1)"
-if [[ "$expect_hash" != "$actual_hash" ]]; then
-  echo "shasum mismatch for efm-langserver. Aborting."
-  exit 1
-fi
+file_name=efm-langserver-${version}.tar.gz
+
+source "$DOTS/common/download.sh"
+cache_file "$file_name" "$download_url" "$expect_hash"
+
 mkdir -p ~/.efm
-tar -C ~/.efm --extract -z -f "${tmpfile}" --strip-components 1
+tar -C ~/.efm --extract -z -f "$(cache_path "${file_name}")" --strip-components 1
 
--- rust/install.sh
+++ rust/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+# rustup-init might change with a new version that is called "stable"
+# to support caching and not get stuck with the old version in the cache,
+# introduce an artificial version
+artificial_version=1
+
 if [ "$(uname -s)" = "Darwin" ]; then
   download_url="https://static.rust-lang.org/rustup/dist/x86_64-apple-darwin/rustup-init"
   expect_hash="203dcef5a2fb0238ac5ac93edea8207eb63ef9823a150789a97f86965c4518f2"
@@ -9,16 +14,12 @@ elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   expect_hash="241a99ff02accd2e8e0ef3a46aaa59f8d6934b1bb6e4fba158e1806ae028eb25"
 fi
 
-tmpdir=$(mktemp -d)
-rustupinit=${tmpdir}/rustup-init
-# shellcheck disable=SC2064
-trap "rm -rf ${tmpdir}" EXIT
-curl -Lo "${rustupinit}" "${download_url}"
-actual_hash="$(shasum -a 256 "${rustupinit}" | cut -d' ' -f 1)"
-if [[ "$expect_hash" != "$actual_hash" ]]; then
-  echo "shasum mismatch for rustup-init. Aborting."
-  exit 1
-fi
+file_name=rustup-init-${artificial_version}
+
+source "$DOTS/common/download.sh"
+cache_file "$file_name" "$download_url" "$expect_hash"
+
+rustupinit="$(cache_path "${file_name}")"
 chmod +x "${rustupinit}"
 "${rustupinit}" -y --verbose --component rust-analyzer
 # shellcheck disable=SC1091
