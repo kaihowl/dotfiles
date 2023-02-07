07dbd1ca1541836f7c21b354235f6a9d4a3d7837
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2023-02-07 07:54:28 +0100

    fix up linter

--- git/test.sh
+++ git/test.sh
@@ -7,10 +7,10 @@ which git
 echo "Ensure git is >= 2.34.0 to support ssh signing"
 git --version
 version=$(git --version 2>&1 | cut -d' ' -f3)
-echo $version
+echo "${version}"
 
 # If version is smaller than 2.34.0
-if [ $(echo "${version}\n2.34.0" | sort -V | head -n 1) != "2.34.0" ]; then
+if [ "$(printf "%s\n2.34.0" "${version}" | sort -V | head -n 1)" != "2.34.0" ]; then
   echo "Version too old, ssh signing not supported"
   echo "Version is ${version}"
   echo "You need at least 2.34.0"
