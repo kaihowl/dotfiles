cb98d064f551eaa29d0fd59d3a8ccaa8bf6f6562
Kai Howelmeyer <kai@hoewelmeyer.eu>
2023-03-12 21:44:05 +0100

    fix(rust): use correct clib type (#537)
    Use the official install script instead to install rust. Directly using
    the rustup-init binaries means I will have to maintain the detection
    logic for OSs, cpu architectures, and clibs.

--- rust/test.sh
+++ rust/test.sh
@@ -5,8 +5,17 @@ set -x
 echo "Check if rustc is on path"
 which rustc
 
+echo "Check if rustc is runnable"
+rustc --version
+
 echo "Check if cargo is on path"
 which cargo
 
+echo "Check if cargo is runnable"
+cargo --version
+
 echo "Check if rust-analyzer is on path"
 which rust-analyzer
+
+echo "Check if rust-analyzer is runnable"
+rust-analyzer --version
