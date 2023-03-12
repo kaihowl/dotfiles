c93533b0f3e9553e46aa59ac1a780b81e9098980
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2023-03-12 21:19:59 +0100

    actual fix

--- rust/install.sh
+++ rust/install.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 set -ex
 
-# rustup-init might change with a new version that is called "stable"
-# to support caching and not get stuck with the old version in the cache,
-# introduce an artificial version
-artificial_version=1
+download_url="https://sh.rustup.rs"
+expect_hash="41262c98ae4effc2a752340608542d9fe411da73aca5fbe947fe45f61b7bd5cf"
 
-if [ "$(uname -s)" = "Darwin" ]; then
-  download_url="https://static.rust-lang.org/rustup/dist/x86_64-apple-darwin/rustup-init"
-  expect_hash="203dcef5a2fb0238ac5ac93edea8207eb63ef9823a150789a97f86965c4518f2"
-elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  download_url="https://static.rust-lang.org/rustup/dist/x86_64-unknown-linux-musl/rustup-init"
-  expect_hash="241a99ff02accd2e8e0ef3a46aaa59f8d6934b1bb6e4fba158e1806ae028eb25"
-fi
-
-file_name=rustup-init-${artificial_version}
+file_name=rustup-sh-1
 
 source "$DOTS/common/download.sh"
 cache_file "$file_name" "$download_url" "$expect_hash"
