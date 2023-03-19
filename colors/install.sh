d3509481899572d90bf06037adf7c700caf274e6
Kai Howelmeyer <kai@hoewelmeyer.eu>
2023-03-19 17:12:43 +0100

    fix(colors): only put change-color on PATH (#553)
    Do not put for example `test.sh` from `colors/` onto the path as well.

--- colors/install.sh
+++ colors/install.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
 # Depends on an installed zsh for using zparseopts in change-color
 "${SCRIPT_DIR}/../zsh/install.sh"
 
-"${SCRIPT_DIR}/../colors/change-color"
+"${SCRIPT_DIR}/../colors/bin/change-color"
