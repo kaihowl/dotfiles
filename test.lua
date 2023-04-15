b1a403da1e5b1e4030e60b30f67b1970ecad4cd2
Kai Hoewelmeyer <kai.hoewelmeyer@tomtom.com>
2023-04-15 22:47:31 +0200

    rel path / clean up

--- test.lua
+++ test.lua
@@ -33,19 +33,17 @@ end
 
 function buffer_commits()
   local comm_path = commit_and_path()
-  local parsing_state = {filename = comm_path[2]}
+  local rel_path = require'fzf-lua'.path.relative(comm_path[2], vim.loop.cwd())
+  local parsing_state = {filename = rel_path}
   require'fzf-lua'.fzf_exec(make_git_log_command(comm_path), {
     fn_transform = function(line)
       if vim.startswith(line, ' rename') then
-        string.match(line, "^ rename ([^{]+){?(.*) => ")
         -- Could be of form 'before.txt => after.txt' OR
         -- the terse 'somedir/{before.txt => after.txt}'
         local dir_or_full, remainder = string.match(line, "^ rename ([^{]+){?(.*) => ")
         remainder = remainder or ''
-        -- return vim.inspect(rename)
-        return dir_or_full .. remainder
-        -- return string.match(line, "%d+")
-        -- return -- not an output line
+        parsing_state.filename = dir_or_full .. remainder
+        return -- not an output line
       end
       if line == '' or vim.startswith(line, ' ') then
         -- discard other summary lines
