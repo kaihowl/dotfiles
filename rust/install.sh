f4f3a73c8182dac072d250f03b4df2159e9488e5
Kai Hoewelmeyer <75625827+KaiHoewelmeyer-TomTom@users.noreply.github.com>
2023-04-30 12:06:04 +0200

    fix(rust): update rust up hash (#615)
    Fixes #613

--- rust/install.sh
+++ rust/install.sh
@@ -3,10 +3,11 @@ set -e
 
 SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
 
+version=2
 download_url="https://sh.rustup.rs"
-expect_hash="41262c98ae4effc2a752340608542d9fe411da73aca5fbe947fe45f61b7bd5cf"
+expect_hash="be3535b3033ff5e0ecc4d589a35d3656f681332f860c5fd6684859970165ddcc"
 
-file_name=rustup-sh-1
+file_name=rustup-sh-${version}
 
 source "${SCRIPT_DIR}/../common/download.sh"
 cache_file "$file_name" "$download_url" "$expect_hash"
