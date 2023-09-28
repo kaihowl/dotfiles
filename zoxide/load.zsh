3d2ef8eeae20e7199b49ebcf3d57df05babae497
Kai Hoewelmeyer <kai.hoewelmeyer@tomtom.com>
2023-09-28 16:38:09 +0200

    disable zoxide

--- zoxide/load.zsh
+++ zoxide/load.zsh
@@ -1,4 +1,4 @@
-# Ensure that the jumped-to folder is echoed
-export _ZO_ECHO=1
-
-eval "$(zoxide init --cmd j zsh)"
+# # Ensure that the jumped-to folder is echoed
+# export _ZO_ECHO=1
+#
+# eval "$(zoxide init --cmd j zsh)"
