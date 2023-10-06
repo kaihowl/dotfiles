5392086187b43b4b7dd28049c442451cdb34a2c0
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2023-10-06 20:14:12 +0200

    feat(zsh): minimal zshrc using p10k
    Fixes #671
    
    accept-perf: zsh

--- zsh/directories.zsh
+++
@@ -1,2 +0,0 @@
-# Undo some settings from lib/directories of oh-my-zsh
-unsetopt auto_name_dirs
--- zsh/install.sh
+++ zsh/install.sh
@@ -3,8 +3,6 @@ set -e
 
 SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
 
-checkout_path=~/.oh-my-zsh
-
 cd "$(dirname "$0")"
 
 if [ "$(uname -s)" = "Darwin" ]; then
@@ -14,17 +12,6 @@ elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   source "${SCRIPT_DIR}/../common/apt.sh"
   # Install 'expect' for testing
   apt_install zsh expect
-fi
-
-if [ -d "$checkout_path" ]; then
-  if [[ -z "${ZSH:-}" ]]; then
-    # Older zsh templates did not export ZSH var
-    export ZSH=$checkout_path
-  fi
-  zsh -i -e -c "omz update --unattended"
-else
-  ../git/install.sh
-  git clone https://github.com/ohmyzsh/ohmyzsh.git $checkout_path
 fi
 
 if (grep -q '/bin/zsh' /etc/shells) && [[ -x /bin/zsh ]]; then
