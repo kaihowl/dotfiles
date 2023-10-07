352b9f2b17324af9211eb01e009f623ac562d02e
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2023-10-07 14:51:07 +0200

    working state

--- zsh/completion_style.zsh
+++ zsh/completion_style.zsh
@@ -5,12 +5,6 @@ export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=
 zstyle ':completion:*:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' menu yes select
 
-# shift-tab reverse menu completion
-# shellcheck disable=SC2154
-if [[ -n "${terminfo[kcbt]}" ]]; then
-  bindkey "${terminfo[kcbt]}" reverse-menu-complete
-fi
-
 # Consider e.g. slashes to be word boundaries
 autoload -U select-word-style
 select-word-style bash
