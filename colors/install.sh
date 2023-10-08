972cf9c583965a6d5eb35985ef3cc8bac49ec96b
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2023-10-08 14:59:42 +0200

    refactor(colors): only install zsh if it is missing

--- colors/install.sh
+++ colors/install.sh
@@ -4,6 +4,8 @@ set -e
 SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
 
 # Depends on an installed zsh for using zparseopts in change-color
-"${SCRIPT_DIR}/../zsh/install.sh"
+if ! command -v zsh > /dev/null; then
+  "${SCRIPT_DIR}/../zsh/install.sh"
+fi
 
 "${SCRIPT_DIR}/../colors/bin/change-color"
