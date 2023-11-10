a03954463bcca89398ea0f5be2a4ec4ab101c964
Kai Hoewelmeyer <75625827+KaiHoewelmeyer-TomTom@users.noreply.github.com>
2023-11-10 09:43:12 +0100

    fix(system): remove lnav (#705)
    I am not using it. It builds dependencies from scratch in homebrew.
    Let's save some time.

--- system/install.sh
+++ system/install.sh
@@ -5,12 +5,12 @@ SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
 
 if [ "$(uname -s)" = "Darwin" ]; then
   source "${SCRIPT_DIR}/../common/brew.sh"
-  brew_install tree jq htop automake libtool pkg-config lnav
+  brew_install tree jq htop automake libtool pkg-config
   source "${SCRIPT_DIR}/../common/utilities.sh"
   if ! version_less_than "$(darwin_version)" 11.0.0 ; then
     brew_install ncdu
   fi
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   source "${SCRIPT_DIR}/../common/apt.sh"
-  apt_install ncdu tree jq htop automake libtool pkg-config lnav
+  apt_install ncdu tree jq htop automake libtool pkg-config
 fi
