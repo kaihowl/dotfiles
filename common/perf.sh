caa56fd928d0eaf7d0076a9383dfbbac1491bef8
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-01-06 00:01:13 +0100

    try simplification

--- common/perf.sh
+++ common/perf.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-mkdir -p ~/.git-perf/
-pushd ~/.git-perf
-if [ "$(uname)" == "Darwin" ]; then
-  curl -L https://github.com/kaihowl/git-perf/releases/download/0.14.0/gitperf-0.14.0-x86_64-apple-darwin.tar.gz | tar -xz --strip-components=1
-elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  curl -L https://github.com/kaihowl/git-perf/releases/download/0.14.0/gitperf-0.14.0-x86_64-unknown-linux-musl.tar.gz | tar -xz --strip-components=1
-fi
-popd
-
 function add_measurement {
   if [[ $# -ne 2 ]]; then
     echo "Expected name and value for add_measurement"
@@ -19,7 +10,7 @@ function add_measurement {
   name=$1
   value=$2
 
-  ~/.git-perf/git-perf add -m "$name" -k "os=${VERSION_RUNNER_OS}" "$value"
+  git perf add -m "$name" -k "os=${VERSION_RUNNER_OS}" "$value"
 }
 
 function run_measurement {
@@ -30,11 +21,11 @@ function run_measurement {
   fi
 
   name=$1
-  ~/.git-perf/git-perf measure -n 10 -m "$name" -k "os=${VERSION_RUNNER_OS}" -- "${@:2}"
+  git perf measure -n 10 -m "$name" -k "os=${VERSION_RUNNER_OS}" -- "${@:2}"
 }
 
 function publish_measurements {
-  ~/.git-perf/git-perf push
+  git perf push
 }
 
 # Make functions available in called bash scripts as well
