a1ad69c98eadf03d363b53fd71f6001801ee9dea
Kai Howelmeyer <kai@hoewelmeyer.eu>
2024-01-06 15:36:41 +0100

    Replace git-perf python with rust version (#513)

--- common/perf.sh
+++ common/perf.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-python3 -m venv ~/.git-perf
-~/.git-perf/bin/python3 -m pip install git+https://github.com/kaihowl/git-perf.git@latest
-
-
 function add_measurement {
+  # Needed as the -e is not preserved in _called_ scripts
+  set -e
+
   if [[ $# -ne 2 ]]; then
     echo "Expected name and value for add_measurement"
     false
@@ -14,10 +13,13 @@ function add_measurement {
   name=$1
   value=$2
 
-  ~/.git-perf/bin/git-perf add -m "$name" -kv "os=${VERSION_RUNNER_OS}" "$value"
+  git perf add -m "$name" -k "os=${VERSION_RUNNER_OS}" "$value"
 }
 
 function run_measurement {
+  # Needed as the -e is not preserved in _called_ scripts
+  set -e
+
   echo $#
   if [[ $# -lt 2 ]]; then
     echo "Expected name and command for run_measurement"
@@ -25,11 +27,14 @@ function run_measurement {
   fi
 
   name=$1
-  ~/.git-perf/bin/git-perf measure -n 10 -m "$name" -kv "os=${VERSION_RUNNER_OS}" -- "${@:2}"
+  git perf measure -n 10 -m "$name" -k "os=${VERSION_RUNNER_OS}" -- "${@:2}"
 }
 
 function publish_measurements {
-  ~/.git-perf/bin/git-perf push
+  # Needed as the -e is not preserved in _called_ scripts
+  set -e
+
+  git perf push
 }
 
 # Make functions available in called bash scripts as well
