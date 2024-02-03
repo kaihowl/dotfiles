bbf6cb20bc200462083a08db6a92865f471aeb7e
Kai Howelmeyer <kai@hoewelmeyer.eu>
2024-02-03 17:52:28 +0100

    chore(efm): update efm to 0.0.50 (#741)

--- efm/install.sh
+++ efm/install.sh
@@ -3,19 +3,19 @@ set -e
 
 SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
 
-version=0.0.48
+version=0.0.50
 
 if [ "$(uname -s)" = "Darwin" ]; then
   if [ "$(uname -m)" = "arm64" ]; then
     download_url="https://github.com/mattn/efm-langserver/releases/download/v${version}/efm-langserver_v${version}_darwin_arm64.zip"
-    expect_hash="90f6aa5c9d3ac485b4cfc4595319b0466c64a12f894cb0bc05d22bbce7a17ade"
+    expect_hash="97bb371ff716faeaecb8fa0d4b108068a015e5ca4a9fc6ae43dc44900ff39248"
   else
     download_url="https://github.com/mattn/efm-langserver/releases/download/v${version}/efm-langserver_v${version}_darwin_amd64.zip"
-    expect_hash="872f3d080fede08542aab114b58ff1cad8a1abdc2726207398d7c1debb6275ea"
+    expect_hash="1d859101e727493c7180cd39d22f65061148e469468529f7498e8a638fc9664c"
   fi
 elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   download_url="https://github.com/mattn/efm-langserver/releases/download/v${version}/efm-langserver_v${version}_linux_amd64.tar.gz"
-  expect_hash="9aa423f462b564994019dd757fa055a846bc27721172a6cdd64eb62c1e01a358"
+  expect_hash="089689a97b072475cc64d0ce2e4a0472421861c79f4e873d91793a8db1a2eee9"
 fi
 
 file_name=efm-langserver-${version}.tar.gz
