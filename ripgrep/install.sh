d8a3c7cba9680fab920b1585d36ae2dc851d937b
Kai Howelmeyer <kai@hoewelmeyer.eu>
2024-02-03 17:56:44 +0100

    chore(ripgrep): upgrade to ripgrep 14.1.0 on Ubuntu (#742)

--- ripgrep/install.sh
+++ ripgrep/install.sh
@@ -12,10 +12,10 @@ elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
 
   source "${SCRIPT_DIR}/../common/download.sh"
 
-  version=14.0.1
+  version=14.1.0
   file_name=ripgrep_${version}
   download_url=https://github.com/BurntSushi/ripgrep/releases/download/${version}/ripgrep_${version}-1_amd64.deb
-  expect_hash="ab0de69388fde06c872525918572105beed6cee657d08a943e1c98c52a4a5139"
+  expect_hash="78953d5a1c97cb363de0098ff73a7ef33fcae014abd4d62f0da490fe3f58ee94"
 
   cache_file "$file_name" "$download_url" "$expect_hash"
   sudo dpkg -i "$(cache_path "${file_name}")"
