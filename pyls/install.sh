f4e791b176088980118c77f2f9daa2e7027f0ddd
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-06-04 17:48:32 +0200

    refactor(python): make common venv functionality available outside nvim
    Topic:refactor-venv

--- pyls/install.sh
+++ pyls/install.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
 source "${SCRIPT_DIR}/../common/python.sh"
 
 # Install autopep8 to activate optional source formatting in pyls
-install_in_virtualenv "python-lsp-server[all]"
-install_in_virtualenv "python-lsp-black"
+install_in_nvim_virtualenv "python-lsp-server[all]"
+install_in_nvim_virtualenv "python-lsp-black"
--- vint/install.sh
+++ vint/install.sh
@@ -4,5 +4,5 @@ set -e
 SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
 
 source "${SCRIPT_DIR}/../common/python.sh"
-install_in_virtualenv "vim-vint"
+install_in_nvim_virtualenv "vim-vint"
 
