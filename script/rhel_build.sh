9a02b02e50119c01ce608175905aeb61072491cf
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-10-14 10:42:03 +0200

    take the guess work out of the rhel-build

---
+++ script/rhel_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+export GIT_PERF_DISABLED=true
+env
+yum install -y git
+# TODO Check centos
+export XDG_CONFIG_HOME=~/.config
+# Make `infocmp` for nvim healthcheck succeed
+export TERM=dumb
+# Ignore potentially insecure directories on GitHub actions runner.
+export ZSH_DISABLE_COMPFIX=true
+pwd
+cd .dotfiles/
+pwd
+./script/ci.sh
