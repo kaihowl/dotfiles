59163139349bad90f32e10cbd9bc8238e8916b31
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-10-19 17:19:08 +0200

    install ccache with nix

--- ccache/install.sh
+++ ccache/install.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
-
-if [ "$(uname)" == "Darwin" ]; then
-  source "${SCRIPT_DIR}/../common/brew.sh"
-  brew_install ccache
-elif [ -f /etc/redhat-release ]; then
-  yum install -y ccache
-elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  # Installing build-essential to have a working compiler for the test.sh
-  # Not necessary on macOS as a full llvm is installed
-  source "${SCRIPT_DIR}/../common/apt.sh"
-  apt_install ccache build-essential
-fi
-
+/nix/var/nix/profiles/default/bin/nix-env --install ccache
