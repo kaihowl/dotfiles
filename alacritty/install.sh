38fd9467cc8d20c8d1cd6b09341787c9dce4db04
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-10-19 17:25:36 +0200

    lint fix

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
-
 /nix/var/nix/profiles/default/bin/nix-env --install alacritty
 
 if [ "$(uname)" == "Darwin" ]; then
