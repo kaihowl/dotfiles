b6cf39ed9b012fd415241078dbf49351d2cc2b06
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-10-19 17:44:40 +0200

    install ctags via nix

--- ctags/test.sh
+++ ctags/test.sh
@@ -4,5 +4,13 @@ set -e
 echo "Check if (universal-)ctags is available"
 which ctags
 
+echo "Check if ctags is nix controlled"
+actual_path=$(realpath "$(which ctags)")
+if [[ "${actual_path}" != /nix/store/* ]]; then
+  echo "Actual Path: $actual_path"
+  echo Expected ctags to be managed by nix
+  exit 1
+fi
+
 echo "Check if ctags is runnable"
 ctags --version
