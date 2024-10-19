186a368bc94f2a8cd0423c7cffd168f3a8023238
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-10-19 17:43:25 +0200

    cmake via nix

--- cmake/test.sh
+++ cmake/test.sh
@@ -4,6 +4,14 @@ set -e
 echo "Check if cmake is available"
 which cmake
 
+echo "Check if cmake is nix controlled"
+actual_path=$(realpath "$(which cmake)")
+if [[ "${actual_path}" != /nix/store/* ]]; then
+  echo "Actual Path: $actual_path"
+  echo Expected cmake to be managed by nix
+  exit 1
+fi
+
 echo "Check that cmake is installed from llvm apt repo (Ubuntu-only)"
 if [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
   apt-cache policy cmake
