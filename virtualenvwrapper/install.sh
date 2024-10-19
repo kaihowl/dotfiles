48d8c1cae7030905c04792d82bca0a97a0c351d2
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-10-19 17:50:18 +0200

    remaining two migrated

--- virtualenvwrapper/install.sh
+++ virtualenvwrapper/install.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
-
-if [ "$(uname -s)" = "Darwin" ]; then
-  source "${SCRIPT_DIR}/../common/brew.sh"
-  brew_install virtualenvwrapper
-elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  source "${SCRIPT_DIR}/../common/apt.sh"
-  apt_install python3-virtualenvwrapper
-fi
+/nix/var/nix/profiles/default/bin/nix-env --install python312Packages.virtualenvwrapper
 
 source "${SCRIPT_DIR}/../common/utilities.sh"
 # Using "sudo which" to bypass any user PATH prefixed python versions
