cad65757ce8d7bdd739d7ea83fa67e1c9e5006a2
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-10-19 17:48:28 +0200

    install jq and others via nix

--- system/install.sh
+++ system/install.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -e
 
-SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
+/nix/var/nix/profiles/default/bin/nix-env --install tree ja htop ncdu tree
 
-if [ "$(uname -s)" = "Darwin" ]; then
-  source "${SCRIPT_DIR}/../common/brew.sh"
-  brew_install tree jq htop automake libtool pkg-config
-  source "${SCRIPT_DIR}/../common/utilities.sh"
-  if ! version_less_than "$(darwin_version)" 11.0.0 ; then
-    brew_install ncdu
-  fi
-elif [[ "$(lsb_release -i)" == *"Ubuntu"* ]]; then
-  source "${SCRIPT_DIR}/../common/apt.sh"
-  apt_install ncdu tree jq htop automake libtool pkg-config
-fi
+# TODO WHY????
+# automake libtool pkg-config
