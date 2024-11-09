2f62c61bf288f8432d02c0e0e108190710fb5f08
Kai Howelmeyer <kai@hoewelmeyer.eu>
2024-11-09 22:44:24 +0100

    fix(revup): do not overwrite the PR body by default (#816)
    topic: revup-no-pr

--- revup/test.sh
+++ revup/test.sh
@@ -3,3 +3,11 @@ set -e
 
 echo "Check that 'revup' is on the path"
 which revup
+
+echo "Check that the pr body is not updated by default"
+current_config=$(revup --verbose upload --dry-run | grep 'update_pr_body')
+if [[ $current_config != *false* ]]; then
+  echo "Expected revup to not update the PR body on upload"
+  echo "Current config is: '$current_config'"
+  exit 1
+fi
