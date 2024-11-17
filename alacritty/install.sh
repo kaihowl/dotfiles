39d2097e55f234f7d09cd002175a8d13bdd34931
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-11-17 12:49:46 +0100

    fix(alacritty): macOS install cannot contain symlinks
    topic:alacritty-install-fix

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -3,5 +3,5 @@ set -e
 
 if [ "$(uname)" == "Darwin" ]; then
   # Actual copy needed, otherwise Spotlight will not show the app
-  sudo cp -rf ~/.nix-profile/Applications/Alacritty.app /Applications/
+  sudo cp -rfL ~/.nix-profile/Applications/Alacritty.app /Applications/
 fi
