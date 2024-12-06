9a3cb52d5143a7327add7b75bc16a4231c219019
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-12-06 08:24:46 +0100

    establish ssh config again

---
+++ ssh/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$(unset CDPATH; cd "$(dirname "$0")" > /dev/null; pwd -P)
+
+source "${SCRIPT_DIR}/../common/utilities.sh"
+
+touch ~/.ssh/config
+sed_replace_in_file ~/.ssh/config "# dotfiles customization" "include ~/.ssh/config_dotfiles # dotfiles customization"
