4e805b6ed42437486e0e2dc4dd0e1153820abb26
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-12-19 18:47:55 +0100

    fix(nvim): enable two flaky tests again
    topic:fixnvim-enable-two-flaky-tests-again

--- nvim/test.sh
+++ nvim/test.sh
@@ -67,10 +67,8 @@ run_vim_test editorconfig.test.vim
 # TODO(kaihowl) flaky (various tests)
 # run_vim_test fzf-git.test.vim
 if [[ $DOTFILES_PROFILE != minimal ]]; then
-  # TODO(kaihowl) disabled failure
-  # run_vim_test completion.test.vim
-  # TODO(kaihowl) disabled
-  # run_vim_test lsp-completion-cpp.test.vim
+  run_vim_test completion.test.vim
+  run_vim_test lsp-completion-cpp.test.vim
   run_vim_test lsp-completion-rust.test.vim
 fi
 run_vim_test lsp-completion-python.test.vim
