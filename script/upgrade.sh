25218983dbfa79fc42a26f98540d5c0d78249181
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2024-12-30 16:37:03 +0100

    fix(script): check branch in DOTS repo, not current working directory
    topic:fixscript-check-branch-in-dots-repo-not-current-working-directory

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env zsh
 # Shamelessly copied from oh my zsh
+
+cd "$DOTS" || exit 1
+
 upstream_branch=$(git rev-parse --abbrev-ref --symbolic-full-name '@{u}' || echo 'NO BRANCH') 
 if [[ $upstream_branch != 'origin/master' ]]; then
   printf '\033[0;31m%s\033[0m\n' 'Not on master branch. Failing auto update.'
   exit 0
 fi
 
-
 printf '\033[0;34m%s\033[0m\n' "Upgrading Dotfiles"
 
 printf '\033[0;34m%s\033[0m\n' 'Requesting sudo for bootstrap/install.'
 sudo /usr/bin/true
 
-cd "$DOTS" || exit 1
 oldcommit=$(git rev-parse master)
 if git fetch origin && git rebase origin/master master
 then
