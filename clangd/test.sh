9d81af135047955cab6e703cc3e49ed237ac0879
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2025-04-08 08:45:06 +0200

    more debugging crutches

--- clangd/test.sh
+++ clangd/test.sh
@@ -14,7 +14,7 @@ echo "Check that clangd is nix managed"
 actual_path=$(realpath "$(which clangd)")
 if [[ "${actual_path}" != /nix/store/* ]]; then
   echo "Actual Path: $actual_path"
-  echo "All paths: $PATH"
+  echo "TODO(kaihowl) All paths: $PATH"
   echo Expected clangd to be managed by nix
   exit 1
 fi
