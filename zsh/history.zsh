70aa18eb76dff0a81ff9e46e5f517caf63dcd087
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2025-05-06 09:18:11 +0200

    feat(zsh): manage bigger history
    topic:featzsh-manage-bigger-history

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -6,3 +6,8 @@ setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt inc_append_history     # add commands to HISTFILE in order of execution
 setopt share_history          # share command history data
+
+HISTFILE=${ZDOTDIR:-$HOME}/.zsh_history
+HISTSIZE=10000
+# shellcheck disable=SC2034
+SAVEHIST=10000
