f9c5de6187321d26e5d1096d4ff6e405f1058f29
Kai Howelmeyer <kai@hoewelmeyer.eu>
2025-06-08 14:41:33 +0200

    feat(zsh): enhance completion and autocomplete settings (#955)
    Updated zsh completion styles with improved menu selection and
    autocomplete configurations, including custom prompts and error
    handling.
    
    This addresses problems when using the dotfiles in a vscode-sized
    terminal with potentially too little space to show all options.
    
    topic:autocomplete-settings

--- zsh/completion_style.zsh
+++ zsh/completion_style.zsh
@@ -3,7 +3,15 @@
 export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 # shellcheck disable=SC2154
 zstyle ':completion:*:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' menu yes select
+
+# Menu selection settings
+zstyle ':completion:*' menu yes select=10
+zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
+zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
+zstyle ':completion:*' max-errors 3
+
+# Autocomplete settings
+zstyle ':autocomplete:*' max-delay 0.5
 
 # shift-tab reverse menu completion
 # shellcheck disable=SC2154
