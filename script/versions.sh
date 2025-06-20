82b84dcf804eb509f29d54ba09d9654649cae0a4
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2025-06-20 15:05:17 +0200

    fix: disable version reporting
    The reporting of vim plugins was broken since the switch to nix. With
    statically determined versions, there is no need to report versions in
    the first place.
    
    topic:kaihowlstack-fix-disable-version-reporting

--- script/versions.sh
+++ script/versions.sh
@@ -8,18 +8,10 @@ fi
 output_file=$1
 echo "output to $output_file"
 
-echo "" > "$output_file"
-
-# TODO use nix-env reporting
-
 # Need to have proper PATH
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
   # shellcheck disable=SC1091
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
 
-tmp_file=$(mktemp)
-trap 'rm -rf $tmp_file' EXIT
-nvim --headless "+PlugSnapshot! $tmp_file" +qall
-
-{ echo "nvim plugins installed:"; cat "$tmp_file"; } >> "$output_file"
+# TODO use nix-env reporting
