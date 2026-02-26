b59a6e52fd18abeb78ebc4bd9c879f3da45030cc
Kai Hoewelmeyer <kai@hoewelmeyer.eu>
2026-02-26 17:05:48 +0100

    fix: non PPA install for yammi and gdb/clang needed

--- gdb/install.sh
+++ gdb/install.sh
@@ -8,10 +8,11 @@ if [[ "$(lsb_release -i 2> /dev/null)" == *"Ubuntu"* ]]; then
   # This will break if it is a different version used for the standard library.
   # We have a test to detect this.
   codename=$(lsb_release -cs)
-  apt_add_repo ubuntu-toolchain-test https://ppa.launchpadcontent.net/ubuntu-toolchain-r/test/ubuntu ::codename:: 60C317803A41BA51845E371A1E9377A2BA9EF27F
   if [[ "$codename" == *"focal"* ]]; then
+    apt_add_repo ubuntu-toolchain-test https://ppa.launchpadcontent.net/ubuntu-toolchain-r/test/ubuntu ::codename:: 60C317803A41BA51845E371A1E9377A2BA9EF27F
     apt_install clang libstdc++6-10-dbg
   elif [[ "$codename" == *"jammy"* ]]; then
+    # Ubuntu 22.04 ships GCC 12 natively; no PPA needed.
     apt_install clang libstdc++6-12-dbg
   else
     echo "Unsupported Ubuntu version $codename"
