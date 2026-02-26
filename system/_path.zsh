dc4b01ab620c38bcace26ae122dbcb68fc0c2816
Kai Howelmeyer <kai@hoewelmeyer.eu>
2026-02-26 19:02:38 +0100

    chore: update current settings (#995)

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,3 @@
 # This can be used to do a final fudging on the PATH
 # This file is loaded _after_ the initial loop over the path.zsh files.
-export PATH="$DOTS/bin:$PATH"
+export PATH="$DOTS/bin:$HOME/.local/bin:$PATH"
